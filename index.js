1754d699d506443c96724210efe092f0d99f5afb
henriB <henri.bretseau@gmail.com>
2020-11-18 18:57:48 +0100

    app use body-parser

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require("./models/dbConfig")
 const postsRoutes = require('./routes/postsController');
 const bodyParser = require("body-parser");
 
+app.use(bodyParser.json())
 app.use('/posts', postsRoutes);
 
 app.listen(5500, ()=> console.log('Serveur started : 5500'));
